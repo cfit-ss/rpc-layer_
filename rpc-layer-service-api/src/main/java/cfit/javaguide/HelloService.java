27938a2a24e18097294d7d3e6db7db48cd07de61
cfit-ss <85338848+cfit-ss@users.noreply.github.com>
2022-09-23 09:57:34 +0800

    Add files via upload

---
+++ rpc-layer-service-api/src/main/java/cfit/javaguide/HelloService.java
@@ -0,0 +1,10 @@
+package cfit.javaguide;
+
+
+/**
+ * @author shengs
+ *
+ */
+public interface HelloService {
+    String hello(Hello hello) throws InterruptedException;
+}
---
+++ rpc-layer-service-api/src/main/java/cfit/javaguide/UserService.java
@@ -0,0 +1,6 @@
+package cfit.javaguide;
+
+public interface UserService {
+
+    User getUsers(int id) throws InterruptedException;
+}
